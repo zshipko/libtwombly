dac632a323326639f231028951314803f14fa389
Zach Shipko <zachshipko@gmail.com>
2015-05-23 01:35:22 -0700

    update tests

--- tests/main.cpp
+++ tests/main.cpp
@@ -52,13 +52,62 @@ const lest::test drawing_test[] = {
         d.setColor(255, 255, 255);
         d.moveTo(10, 10);
         d.lineTo(490, 490);
-        d.stroke();
 
         int pathid = d.activePath();
 
-        d.newPath();
+        EXPECT (d.totalVertices() == 2);
 
-        EXPECT( pathid != d.activePath());
+        double x, y;
+        d.getVertex(1, &x, &y);
+        EXPECT(x == 490);
+        EXPECT(y == 490);
+
+        d.modifyVertex(1, 248, 249);
+        d.getVertex(1, &x, &y);
+        EXPECT(x == 248);
+        EXPECT(y == 249);
+
+        d.newPath();
+        EXPECT (d.activePath() != pathid);
+        EXPECT (d.activePath() > pathid);
+
+        d.newPath();
+        d.lineWidth(2);
+        d.ellipse(250, 250, 100, 100);
+        d.stroke();
+
+        d.activePath(pathid);
+        d.stroke();
+
+        namedWindow("Path test");
+        imshow("Path test", im);
+        waitKey(0);
+        destroyAllWindows();
+    },
+
+    CASE ( "Curve" ) {
+        Mat3b im2(500, 500);
+        im2.setTo(Scalar(255, 255, 255));
+        auto d = draw(im2);
+
+        d.setColor(255, 0, 0);
+        d.moveTo(100, 100);
+        d.curveTo(400, 400, 100, 320);
+        d.stroke();
+
+        namedWindow("Curve test");
+        imshow("Curve test", im2);
+        waitKey(0);
+
+        d.newPath();
+        d.clear(255, 255, 255);
+        d.setColor(0, 255, 0);
+        d.moveTo(100, 100);
+        d.curveTo(400, 400, 150, 320);
+        d.stroke();
+        imshow("Curve test", im2);
+        waitKey(0);
+        destroyAllWindows();
     }
 
 };
